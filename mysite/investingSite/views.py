1017dd44ae4ffc1a36f8ecfbae5b6400133a4f48
Oystein <oebars@gmail.com>
2020-05-14 22:44:37 +0200

    Added some more stuff to get started

--- mysite/investingSite/views.py
+++ mysite/investingSite/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'investingSite/index.html'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return HttpResponse("Put the list of top investments here")
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'investingSite.apps.InvestingsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

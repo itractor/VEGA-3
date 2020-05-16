7390d8a3772ae85c6882a1707574e3a4dc3c1fa5
Oystein <oebars@gmail.com>
2020-05-16 18:19:02 +0200

    Functional list of stocks with no details

--- mysite/investingsite/views.py
+++ mysite/investingsite/views.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,7 +14,8 @@ class IndexView(generic.ListView):
     context_object_name = 'stock_list'
 
     def get_queryset(self):
+        """F
+        Return the last five published questions (not including those set to be
+        published in the future).
         """
-        Return a stock
-        """
-        return Stock
+        return Stock.objects.all()

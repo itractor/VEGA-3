aff963ff96c2afdd5605bb9a9275710297e5f13a
Oystein <oebars@gmail.com>
2020-05-28 23:49:37 +0200

    Updated test and added migrations

--- vega/investingsite/models.py
+++ vega/investingsite/models.py
@@ -14,12 +14,16 @@ class Stock(models.Model):
     stock_id = models.CharField(primary_key=True, max_length=20, default='UNDEFINED')
     stock_name = models.CharField(max_length=100)
     stock_ticker = models.CharField(max_length=10)
-    stock_price = models.DecimalField(decimal_places=3, max_digits=10, default=0)
-    stock_earnings = models.DecimalField(decimal_places=3, max_digits=10, default=0)
+    stock_price = models.DecimalField(decimal_places=3, max_digits=100, default=0)
+    stock_earnings = models.DecimalField(decimal_places=3, max_digits=100, default=0)
+    stock_shares_outstanding = models.BigIntegerField(default=1)
 
     def __str__(self):
         return self.stock_name
 
     def pricetoearnings(self):
 
-        return self.stock_price / self.stock_earnings
+        return self.stock_price / Stock.earnings_per_share(self)
+
+    def earnings_per_share(self):
+        return self.stock_earnings / self.stock_shares_outstanding
--- vega/investingsite/tests.py
+++ vega/investingsite/tests.py
@@ -5,14 +5,14 @@ from decimal import *
 from .models import Stock
 
 
-def create_stock(stockid, stockname, ticker, price, earnings):
+def create_stock(stockid, stockname, ticker, price, earnings, sharesoutstanding):
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
     return Stock.objects.create(stock_id=stockid, stock_name=stockname, stock_ticker=ticker, stock_price=price,
-                                stock_earnings=earnings)
+                                stock_earnings=earnings, stock_shares_outstanding=sharesoutstanding)
 
 
 # Create your tests here.
@@ -28,6 +28,6 @@ class StockIndexViewTests(TestCase):
         """
         If no questions exist, an appropriate message is displayed.
         """
-        stock = create_stock("MOWIXOSL2019", "MOWI", "MOWI", Decimal('12.9'), Decimal('12.9'))
+        stock = create_stock("MOWIXOSL2019", "MOWI", "MOWI", Decimal('70.9'), Decimal('12900.0'), 1000)
         print(stock.pricetoearnings())
-        self.assertEqual(stock.pricetoearnings(), 1)
+        self.assertEqual(stock.pricetoearnings(), 100000000)
